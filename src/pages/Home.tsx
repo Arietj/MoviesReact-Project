0d904fde7c24c2b094755c7cd0e3d26a7b753dc0
Ariet <arietbakytbekov39@gmail.com>
2026-04-23 00:51:24 +0600

    Почти сделан проект

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,38 +1,34 @@
-import { useState } from "react";
-import { searchMovies } from "../services/api";
+import { useEffect, useState } from 'react';
+import { boardMovies, searchMovies } from '../services/api';
+import SearchBar from '../components/SearchBar';
+import MoviesList from '../components/MoviesList';
 
-export default function Home() {
-  const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
+    export default function Home() {
+        const [movies, setMovies] = useState([]);
 
-  const handleSearch = () => {
-    searchMovies(query).then((data) => {
-      console.log(data.titles);
-      setMovies(data.titles); 
-    });
-  };
+        useEffect (() =>{
+            boardMovies().then(data => {
+                setMovies(data.titles);
+            });
+        }, []);
 
-  return (
-    <div>
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
+        const handleSearch = (query) => {
+            searchMovies(query).then(data => {
+                setMovies(data.titles);
+            });
+        };
 
-      <button onClick={handleSearch}>Search</button>
+       
 
-      <div className="grid grid-cols-2 gap-2">
-        {movies.map((movie) => (
-          <div key={movie.id} className="p-2">
-            <img
-              src={movie?.primaryImage?.url}
-              alt={movie.originalTitle}
-            />
-            <h3>{movie.originalTitle}</h3>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
+        return (
+            <div className='max-w-6xl mx-auto px-4 py-10'>
+              <div className='mb-12 text-center'>
+                <h1 className='text-4xl font-extrabold text-slate-900 mb-4'>Поиск Популярных Фильмов</h1>
+                <p className='text-lg text-slate-600 mb-8 max-w-2xl mx-auto'>Здесь вы можете найти информацию о фильмах</p>
+                <SearchBar onSearch={handleSearch}/>
+              </div>
+                
+                <MoviesList  movies={movies} />
+            </div>
+        );
+    }
